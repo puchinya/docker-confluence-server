1ae82f8abdc15d9502d303c78fa0e7055a03d64c
masatakanabeshima <xpuchinya@gmail.com>
2017-03-25 12:21:36 +0900

    change

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -f ${CONFLUENCE_INSTALL}/conf/server.xml.org　${CONFLUENCE_INSTALL}/conf/server.xml
+cp -f ${CONFLUENCE_INSTALL}/conf/server.xml.org ${CONFLUENCE_INSTALL}/conf/server.xml
 sed -i -e "4s/%proxyName%/${CONFLUENCE_PROXY_NAME}/" ${CONFLUENCE_INSTALL}/conf/server.xml
 sed -i -e "4s/%proxyPort%/${CONFLUENCE_PROXY_PORT}/" ${CONFLUENCE_INSTALL}/conf/server.xml
 sed -i -e "4s/%scheme%/${CONFLUENCE_SCHEME}/" ${CONFLUENCE_INSTALL}/conf/server.xml
